f4b7c6392d06c9a4c3b00ac64f668993a55ea399
Alex Johnson <alex.johnson@myunidays.com>
2021-10-07 16:06:26 +0100

    Adds scoring
    Co-authored-by: Joshua Richardson <JoshuaR830@users.noreply.github.com>

--- src/score.js
+++ src/score.js
@@ -23,18 +23,76 @@ const calculate = ( dice, category ) => {
         case categories.SIXES:
             filterBy = 6;
             break;
-        case categories.PAIR:
-            let foo = [0,0,0,0,0,0];
+        case categories.PAIR: {
+            const filteredDice = filterDice(dice, 2);
+            return Math.max(...filteredDice.map(x => x.value)) * 2;
+        };
+        case categories.TWO_PAIRS: {
+            const filteredDice = filterDice(dice, 2);
 
-            for(let i = 0; i < dice.length; i ++) {
-                foo[dice[i] - 1]++;
-            }
+            if (filteredDice.length < 2) return 0;
 
-            return (foo.indexOf(2) + 1) * 2;
+            return (filteredDice[0].value + filteredDice[1].value) * 2;
+        };
+        case categories.THREE_OF_A_KIND: {
+            const filteredDice = filterDice(dice, 3);
+            return filteredDice.length ? filteredDice[0].value * 3 : 0;
+        };
+        case categories.FOUR_OF_A_KIND: {
+            const filteredDice = filterDice(dice, 4);
+            return filteredDice.length ? filteredDice[0].value * 4 : 0
+        };
+        case categories.SMALL_STRAIGHT: {
+            const filteredDice = filterDice(dice, 1).filter((die) => die.value !== 6)
+            return filteredDice.length === 5 ? 15 : 0;
+        };
+        case categories.LARGE_STRAIGHT: {
+            const countedDice = countDice(dice);
+
+            const ones = countedDice[0]
+            const twosThreesFoursFives = countedDice.slice(1, 5)
+            const sixes = countedDice[5]
+
+            if(!twosThreesFoursFives.every(x => x.count === 1))
+                return 0;
+
+            return ones.count + sixes.count === 1 ? 40 : 0;
+        };
+        case categories.FULL_HOUSE: {
+            return (filterDice(dice, 3).length === 1) && (filterDice(dice, 2).length > 1) ? 25 : 0;
+        };
+        case categories.YAHTZEE: {
+            return (filterDice(dice, 5).length === 1 ? 50 : 0);
+        };
+        case categories.CHANCE: {
+            return dice.reduce((a, b) => a + b);
+        };
     }
 
     return dice.filter(d => d === filterBy).reduce((d, v) => ( d + v ), 0);
 };
+
+const filterDice = ( dice, filterValue ) => {
+    const countedDice = countDice(dice);
+    return countedDice.filter((die) => die.count >= filterValue);
+};
+
+const countDice = ( dice ) => {
+    let countedDice = [
+        {value: 1, count: 0},
+        {value: 2, count: 0},
+        {value: 3, count: 0},
+        {value: 4, count: 0},
+        {value: 5, count: 0},
+        {value: 6, count: 0}
+    ];
+
+    dice.forEach(die => {
+        countedDice[die - 1].count++;
+    });
+
+    return countedDice;
+}
 
 
 module.exports = calculate;
--- test/score.test.js
+++ test/score.test.js
@@ -53,6 +53,124 @@ describe("Pairs", () => {
     test("Pair sixes", () => {
         expect(calculate([2, 3, 6, 4, 6], categories.PAIR)).toBe(12);
     });
+
+    test("GivenDiceThatContainAPairOfTwosAndPairOfFours_WithACategoryOfPair_ThenTheResultShouldBeEight", () => {
+        expect(calculate([2, 2, 4, 4, 6], categories.PAIR)).toBe(8);
+    });
+
+    test("GivenDiceThatContainAPairOfFivesAndPairOfThrees_WithACategoryOfPair_ThenTheResultShouldBeTen", () => {
+        expect(calculate([5, 5, 3, 3, 6], categories.PAIR)).toBe(10);
+    });
+
+    test("GivenDiceThatContainThreeFivesAndPairOfThrees_WithACategoryOfPair_ThenTheResultShouldBeTen", () => {
+        expect(calculate([5, 5, 3, 3, 5], categories.PAIR)).toBe(10);
+    });
+
+    test("GivenDiceThatContainTwoThreesAndPairOfFours_WithACategoryOfPair_ThenTheResultShouldBeEight", () => {
+        expect(calculate([3, 3, 4, 4, 4], categories.PAIR)).toBe(8);
+    });
 });
 
+describe("GivenACategoryTwoPairs", () => {
+    test("WhenDiceValuesContainAPairOfTwosAndAPairOfFives_ThenTheResultShouldBeFourteen", () => {
+        expect(calculate([5, 2, 3, 5, 2], categories.TWO_PAIRS)).toBe(14);
+    });
 
+    test("WhenDiceValuesContainOnePairsOfTwos_ThenTheResultShouldBeZero", () => {
+        expect(calculate([5, 2, 3, 5, 1], categories.TWO_PAIRS)).toBe(0);
+    });
+
+    test("GivenDiceThatContainTwoThreesAndPairOfFours_ThenTheResultShouldBeFourteen", () => {
+        expect(calculate([3, 3, 4, 4, 4], categories.TWO_PAIRS)).toBe(14);
+    });
+});
+
+describe("Given the category ThreeOfAKind", () => {
+    test("When dice values contain three fours, then the result should be twelve", () => {
+        expect(calculate([3, 3, 4, 4, 4], categories.THREE_OF_A_KIND)).toBe(12);
+    });
+
+    test("When the dice values do not contain three of a kind, then the result should be zero", () => {
+        expect(calculate([1, 2, 4, 6, 4], categories.THREE_OF_A_KIND)).toBe(0);
+    });
+});
+
+describe("Given the catgory is FourOfAKind", () => {
+    test("When dice values contain four fives, then the result should be 20", () => {
+        expect(calculate([3, 5, 5, 5, 5], categories.FOUR_OF_A_KIND)).toBe(20);
+    });
+
+    test("When the dice values do not contain 4 of a kind, then the result should be 0", () => {
+        expect(calculate([3, 3, 5, 5, 5], categories.FOUR_OF_A_KIND)).toBe(0);
+    });
+});
+
+describe("Given the category is SmallStraight", () => {
+    test("When the dice values are 1, 2, 3, 4, 5 then the result shoud be 15", () => {
+        expect(calculate([1, 2, 3, 4, 5], categories.SMALL_STRAIGHT)).toBe(15);
+    });
+
+    test("When the dice values are 2, 3, 4, 5, 6 then the result shoud be 0", () => {
+        expect(calculate([2, 3, 4, 5, 6], categories.SMALL_STRAIGHT)).toBe(0);
+    });
+
+    test("When the dice values are 1, 3, 4, 5, 6 then the result shoud be 0", () => {
+        expect(calculate([1, 3, 4, 5, 6], categories.SMALL_STRAIGHT)).toBe(0);
+    });
+
+    test("When the dice values do not contain a small straight the result shoud be 0", () => {
+        expect(calculate([1, 2, 3, 3, 5], categories.SMALL_STRAIGHT)).toBe(0);
+    });
+});
+
+describe("Given the category is LargeStraight", () => {
+    test("When the dice values are 2, 3, 4, 5, 6 then the result should be 40", () => {
+        expect(calculate([2, 3, 4, 5, 6], categories.LARGE_STRAIGHT)).toBe(40);
+    });
+
+    test("When the dice values are 1, 2, 3, 4, 5 then the result should be 40", () => {
+        expect(calculate([1, 2, 3, 4, 5], categories.LARGE_STRAIGHT)).toBe(40);
+    });
+
+    test("When the dice values are 3, 5, 4, 2, 1 then the result should be 40", () => {
+        expect(calculate([3, 5, 4, 2, 1], categories.LARGE_STRAIGHT)).toBe(40);
+    });
+
+    test("When the dice values are 1, 2, 4, 4, 5 then the result should be 0", () => {
+        expect(calculate([1, 2, 4, 4, 5], categories.LARGE_STRAIGHT)).toBe(0);
+    });
+
+    test("When the dice values are 1, 2, 3, 4, 6 then the result should be 0", () => {
+        expect(calculate([1, 2, 3, 4, 6], categories.LARGE_STRAIGHT)).toBe(0);
+    });
+});
+
+describe("Given the category is FullHouse", () => {
+    test("When the dice values are 2, 2, 3, 3, 3 then the result should be 25", () => {
+        expect(calculate([2, 2, 3, 3, 3], categories.FULL_HOUSE)).toBe(25);
+    });
+
+    test("When the dice values are 4, 4, 4, 4, 4 then the result should be 0", () => {
+        expect(calculate([4, 4, 4, 4, 4], categories.FULL_HOUSE)).toBe(0);
+    });
+
+    test("When the dice values are 1, 3, 2, 5, 5 then the result should be 0", () => {
+        expect(calculate([1, 3, 2, 5, 5], categories.FULL_HOUSE)).toBe(0);
+    });
+});
+
+describe("Given the category is Yahtzee", () => {
+    test("When the dice values are 5, 5, 5, 5, 5 then the result should be 50", () => {
+        expect(calculate([5, 5, 5, 5, 5], categories.YAHTZEE)).toBe(50);
+    });
+
+    test("When the dice values are 4, 4, 4, 4, 5 then the result should be 0", () => {
+        expect(calculate([4, 4, 4, 4, 5], categories.YAHTZEE)).toBe(0);
+    })
+});
+
+describe("Given the category is Chance", () => {
+    test("When the dice values are 4, 5, 3, 6, 4 then the result should be 22", () => {
+        expect(calculate([4, 5, 3, 6, 4], categories.CHANCE)).toBe(22);
+    });
+});
